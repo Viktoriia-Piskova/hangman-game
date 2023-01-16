d7bf9fc5ac74188850d6c27b082220f2d9351336
Viktoriia-Piskova <viktoriapiskova@gmail.com>
2023-01-16 17:11:15 +0200

    Keyboard

---
+++ src/components/HangmanPicture.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <img src="../assets/HangmanImg.svg" class="logo" alt="hang pic" />
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'HangmanPicture'
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/KeyBoard.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <button v-for="letter in letters" :key="letter.value" @click="selectLetter(letter)" :class="{isDisabled: letter.disabled}" :disabled="letter.disabled">
+            {{ letter.value }}
+        </button>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            letters: []
+        }
+    },
+    computed: {
+         
+    },
+    methods: {
+        selectLetter(letter) {
+            letter.disabled = true
+            console.log(letter)
+        }
+    },
+    created() {
+            const charts = Array.from(Array(26).keys()).map((e) => e + 65);
+            const alphabet = charts.map((el) => {return {value: String.fromCharCode(el), disabled: false}})
+            this.letters = [...alphabet]
+    }
+}
+</script>
+
+<style scoped>
+.isDisabled {
+    background-color: red;
+}
+</style>
